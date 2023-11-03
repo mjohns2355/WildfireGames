e9940578e206f469ea975f9da369e57088797a30
mli254 <mli254@ucsc.edu>
2023-11-03 03:12:29 -0700

    implemented rudimentary inventory system

--- Assets/Scripts/FindYourThings/InventoryManager.cs
+++ Assets/Scripts/FindYourThings/InventoryManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     private static InventoryManager _instance; // static private variable of the component data type
 	public static InventoryManager Instance { get { return _instance; } } // public way to access the private variable
+
+    private List<GameObject> items;
+    public TMPro.TextMeshProUGUI inventoryText;
+    public GameObject itemPrefab;
 
 	private void Awake() {
         // if there is already a value assigned to the private variable and its not this, destroy this
@@ -15,16 +21,42 @@ public class InventoryManager : MonoBehaviour
         // if there is no value assigned to the private variable, assign this as the reference
         	_instance = this;
         }	
-        DontDestroyOnLoad(this.gameObject);
+        // DontDestroyOnLoad(this.gameObject); // while working with one scene, commenting out for now
     }
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         
+    }
+
+    public void addItem(string item)
+    {
+        GameObject newItem = Instantiate(itemPrefab, inventoryText.gameObject.GetComponent<RectTransform>());
+        GameObject collectable = GameObject.Find(item);
+
+        collectable.SetActive(false);
+
+        newItem.GetComponent<Button>().onClick.AddListener(() => {
+            newItem.SetActive(false);
+            collectable.SetActive(true);
+            //items.Remove(newItem);
+        });
+
+        TMPro.TextMeshProUGUI itemText = newItem.GetComponent<Button>().GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        itemText.text = item;
+        //items.Add(newItem); // currently this doesn't work
+    }
+
+    private void updatePlacement() 
+    {
+        foreach (GameObject item in items)
+        {
+
+        }
     }
 }
