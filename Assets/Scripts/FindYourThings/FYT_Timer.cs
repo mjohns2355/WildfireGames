3dd1099889c1784fc44622cf4124801f6d095e78
mli254 <mli254@ucsc.edu>
2023-12-01 09:57:32 -0800

    added timer and an end game button

--- Assets/Scripts/FindYourThings/FYT_Timer.cs
+++ Assets/Scripts/FindYourThings/FYT_Timer.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FYT_Timer : MonoBehaviour
 {
+    public GameObject EndMenu;
+    public float timeLimit = 60;
+    public TextMeshProUGUI timerText;
+    private float timePassed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timePassed = timeLimit;
+        UpdateTimeUI(timePassed);   
+    }
+
+    void OnEnable() 
+    {
+        timePassed = timeLimit;
+        UpdateTimeUI(timePassed); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        timePassed -= Time.deltaTime;
+        UpdateTimeUI(timePassed);
+
+        if (timePassed <= 0)
+        {
+            TimerOver();
+        }
         
+    }
+
+    void UpdateTimeUI(float time) 
+    {
+        timerText.text = "Time Remaining: " + Mathf.Max(0, Mathf.Ceil(time));
+    } 
+
+    void TimerOver()
+    {
+        EndMenu.SetActive(true);
+        this.gameObject.SetActive(false);
     }
 }
