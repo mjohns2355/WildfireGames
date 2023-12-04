d2a2d2bba4fb8c4341ccf7eabf1aeb7f19c750e7
mli254 <mli254@ucsc.edu>
2023-12-04 06:21:42 -0800

    removed unused dependencies

--- Assets/Scripts/FindYourThings/FYT_InventoryManager.cs
+++ Assets/Scripts/FindYourThings/FYT_InventoryManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
