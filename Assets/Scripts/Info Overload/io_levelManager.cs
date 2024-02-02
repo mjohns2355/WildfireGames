58753a97552a9ef8f78833019e3a745737f4e50e
moarKitties <99438834+moarKitties@users.noreply.github.com>
2024-02-02 07:20:06 -0800

    Info Overload progress

--- Assets/Scripts/Info Overload/io_levelManager.cs
+++ Assets/Scripts/Info Overload/io_levelManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class io_levelManager : MonoBehaviour
 {
+
+    public GameObject brakes;
+    public GameObject crashScreen;
+    private float brakeStartTimer;
+    private float brakeOnTimer = -1;
+    private bool safe = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        brakeStartTimer = Random.Range(4, 8);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (brakeOnTimer <= -1)
+        {
+            brakeStartTimer -= Time.deltaTime;
+            if (brakeStartTimer <= 0)
+            {
+                safe = false;
+                brakes.SetActive(true);
+                brakeOnTimer = 10;
+            }
+        } else
+        {
+            brakeOnTimer -= Time.deltaTime;
+            if(brakeOnTimer <= 0)
+            {
+                if (safe)
+                {
+                    brakeOnTimer = -1;
+                    brakeStartTimer = Random.Range(4, 8);
+                    brakes.SetActive(false);
+                } else
+                {
+                    crashScreen.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Brake()
+    {
+        safe = true;
+        brakeOnTimer = Random.Range(1, 2);
     }
 }
