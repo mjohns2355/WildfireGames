b08b6110268132aedf8dda6d4f09bc78089fcf5a
Yiyang Lu <lyiyang@ucsc.edu>
2024-05-28 09:29:58 -0700

    added inspector button

--- Assets/WindZone.cs
+++ Assets/WindZone.cs
@@ -4,32 +4,57 @@ using UnityEngine;
 
 public class WindZone : UnitySingleton<WindZone>
 {
-    public float windForce = 0f;
-    public float windSpeed = 0f;
+    [Range(0f, 10f)]
+    [SerializeField] float windForce = 0f;
+    [Range(0f,10f)]
+    [SerializeField] float windSpeed = 0f;
+    [Range(0f,100f)]
+    [SerializeField] float range = 10f;
+    [InspectorButton("ChangeWindRange")]
+    public bool Change;
+    public bool isStill = false;
+    [SerializeField] Vector3 windDirection;
+ 
     Rigidbody rb;
+    BoxCollider collider;
     // Start is called before the first frame update
     void Start()
     {
+        windDirection = transform.right;
         rb = GetComponent<Rigidbody>();
-       
+        collider = GetComponent<BoxCollider>();
+        transform.localScale = new Vector3(range, 8, range);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = windSpeed * transform.right;
+        if (isStill)
+        {
+            rb.velocity = Vector3.zero;
+            return;
+        }
+        rb.velocity = windSpeed * windDirection;
     }
 
+    private void FixedUpdate()
+    {
+        
+    }
     private void OnTriggerStay(Collider other)
     {
         var hit = other.gameObject;
         if (hit != null && hit.layer == LayerMask.NameToLayer("Fire"))
         {
             var fire = hit.GetComponent<FireMovementController>();
-            fire.windDirection = transform.right;
+            fire.windDirection = windDirection;
             fire.speed = windForce;
             fire.ImpactFire(windForce);
         }
     }
 
+    private void ChangeWindRange()
+    {
+        transform.localScale = new Vector3(range, 8,  range);
+    }
 }
