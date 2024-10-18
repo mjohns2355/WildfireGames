d8e67e782b5e20bc8f4d8d690f987bcc4fc39369
Yiyang Lu <102345976+lyiyang0702@users.noreply.github.com>
2024-10-18 11:42:30 -0700

    WIP

---
+++ Assets/Scripts/Resilient Resident/UI/StorePanel.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorePanel : MonoBehaviour
+{
+    public GameObject owned;
+    public GameObject available;
+}
