ae8905e93d83ef6c4e6d9d921a01bb091688e15c
Yiyang Lu <102345976+lyiyang0702@users.noreply.github.com>
2025-05-09 15:26:32 -0700

    Update StructureContextMenu.cs

--- Assets/Scripts/FirewiseCitizen/UI/StructureContextMenu.cs
+++ Assets/Scripts/FirewiseCitizen/UI/StructureContextMenu.cs
@@ -61,7 +61,7 @@ public class StructureContextMenu : MonoBehaviour
             isSelected = true;
 
             GameManager.Instance.cameraMovement.ResetCam();
-            GameManager.Instance.currentStage = LevelStage.PhaseOne;
+            //GameManager.Instance.currentStage = LevelStage.PhaseOne;
             OnMenuDisable();
         });
 
