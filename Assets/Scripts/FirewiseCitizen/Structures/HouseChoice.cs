2ecdfe8040c2e39aa655df099a3aaeaf0f8d0273
Yiyang Lu <lyiyang@ucsc.edu>
2025-05-16 10:16:21 -0700

    Update HouseChoice.cs

--- Assets/Scripts/FirewiseCitizen/Structures/HouseChoice.cs
+++ Assets/Scripts/FirewiseCitizen/Structures/HouseChoice.cs
@@ -15,6 +15,9 @@ public class HouseChoice
     [TextArea(5, 10)]
     public string choiceDetail;
     [TextArea(5, 10)]
+    public string choiceThought;
+    public Sprite choiceThoughtSprite;
+    [TextArea(5, 10)]
     public string endGameFeedback;
     public bool isLocked = false;
     public bool isNormal;
