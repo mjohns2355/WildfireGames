6fb469fc2075c382abb9af4e9c8ce1a76db03f32
Yiyang Lu <102345976+lyiyang0702@users.noreply.github.com>
2025-05-19 10:21:25 -0700

    fixed player text and tree collision

--- Assets/Scripts/FiresafeFriend/HouseSystem/HouseManager.cs
+++ Assets/Scripts/FiresafeFriend/HouseSystem/HouseManager.cs
@@ -70,6 +70,7 @@ namespace HappyHouse.HouseSystem
             //{
             //    InitHouseNode(nodeDictionary, f.GetComponent<BaseHousePartObject>());
             //}
+            var name = playerTag == "P1" ? "Player 1" : "Player 2";
             nameText.GetComponent<TextMeshPro>().text = playerTag;
 
             for (int i = 0; i < transform.childCount; i++)
