ebf648d84e4607b02f2620d35ce98e04240f489a
tristynl <tristyn-lai@outlook.com>
2025-05-30 10:33:10 -0700

    FF Resources Updated

--- Assets/Scripts/FirewiseCitizen/UI/ResourceWebsite.cs
+++ Assets/Scripts/FirewiseCitizen/UI/ResourceWebsite.cs
@@ -2,26 +2,21 @@ using UnityEngine;
 
 public class ResourceWebsite : MonoBehaviour
 {
-    /*public string url;
+    //FF
 
-    public void OpenLink()
+    public void ActionPlan()
     {
-        Application.OpenURL(url);        
-    }*/
-
-    public void Calfire()
-    {
-        Application.OpenURL("https://www.fire.ca.gov/");        
+        Application.OpenURL("https://34c031f8-c9fd-4018-8c5a-4159cdff6b0d-cdn-endpoint.azureedge.net/-/media/calfire-website/about/communications/before-during-and-after.pdf?rev=a7a4e48e07d049b186f9f281a252bdea&hash=22A288EBAB74F59EBD844E129ACC225A");        
     }
 
-    public void FireSafeMarin()
+    public void Vegetation()
     {
-        Application.OpenURL("https://firesafemarin.org/");        
+        Application.OpenURL("https://www.fire.ca.gov/what-we-do/natural-resource-management/vegetation-management-program");        
     }
 
-    public void WildfireMarin()
+    public void DefensibleSpace()
     {
-        Application.OpenURL("https://www.marinwildfire.org/");        
+        Application.OpenURL("https://34c031f8-c9fd-4018-8c5a-4159cdff6b0d-cdn-endpoint.azureedge.net/-/media/calfire-website/about/communications/defensible-space-flyer.pdf?rev=1d253e5a73f64b5daead022b176b0660&hash=01A3A362B30B8B56051317E4EAED31BC");        
     }
 
 }
