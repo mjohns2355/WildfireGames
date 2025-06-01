caafd93a8a59823b3865ce64293994ed4202eee0
tristynl <tristyn-lai@outlook.com>
2025-06-01 00:13:36 -0700

    FC - Got Rid of Fetcher + Links Moved

--- Assets/Scripts/FirewiseCitizen/UI/ResourceWebsite.cs
+++ Assets/Scripts/FirewiseCitizen/UI/ResourceWebsite.cs
@@ -19,4 +19,38 @@ public class ResourceWebsite : MonoBehaviour
         Application.OpenURL("https://34c031f8-c9fd-4018-8c5a-4159cdff6b0d-cdn-endpoint.azureedge.net/-/media/calfire-website/about/communications/defensible-space-flyer.pdf?rev=1d253e5a73f64b5daead022b176b0660&hash=01A3A362B30B8B56051317E4EAED31BC");        
     }
 
+    //FR
+
+    public void Pet()
+    {
+        Application.OpenURL("https://34c031f8-c9fd-4018-8c5a-4159cdff6b0d-cdn-endpoint.azureedge.net/-/media/calfire-website/about/communications/wildland-fire-safety-for-your-livestock-and-pets.pdf?rev=d6ca645c17644811aa6e371d9a258306&hash=D66622EFE80EB060D3E8A6D2612D5B5E");        
+    }
+
+    public void Early()
+    {
+        Application.OpenURL("https://www.fire.ca.gov/prepare/get-ready-to-go");        
+    }
+
+    public void WildfirePreventionGrant()
+    {
+        Application.OpenURL("https://www.fire.ca.gov/what-we-do/grants/wildfire-prevention-grants");        
+    }
+
+    //FYT
+
+    public void Evacuation()
+    {
+        Application.OpenURL("https://www.fire.ca.gov/prepare/get-set");        
+    }
+
+    public void GetAlerts()
+    {
+        Application.OpenURL("https://plan.readyforwildfire.org/en");        
+    }
+
+    public void GoBag()
+    {
+        Application.OpenURL("https://readyforwildfire.org/prepare-for-wildfire/emergency-supply-kit/");        
+    }
+
 }
