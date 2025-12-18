37fe80d206d57b6f30d7dc661dcca460ad4aae93
tristynl <tristyn-lai@outlook.com>
2025-12-18 01:28:41 -0800

    FF HomeHardening Portion Fixed

--- Assets/Scripts/FiresafeFriend/Tutorial/FF_TutorializedHousePart.cs
+++ Assets/Scripts/FiresafeFriend/Tutorial/FF_TutorializedHousePart.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+
 public class FF_TutorializedHousePart : FF_TutorializedObject
 {
     public HousePartType partType;
@@ -12,14 +13,25 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
     public HighlightMesh highlightMesh;
     public GameObject plantModeToggle;
     public Transform camH1;
+
     PurchaseFloatingButton bubble;
     HouseManager houseManager;
     List<BaseHousePartObject> partObjects;
     InventoryUI inventory;
     InventoryItem oldItem, newItem;
+
+    public string inventoryIntroKey = "tutText11";
+    public string inventoryReplaceKey = "tutText12";
+    public string inventoryCompleteKey = "tutText14";
+    public string inventoryRetryKey = "tutText13";
+    public string toggleHouseModeKey = "tutText9";
+    public string upgradeRoofIntroKey = "tutText10";
+    public string purchaseMaterialKey = "tutText9";
+
     public override void OnTutorialStepStart()
     {
         inventory = HH_GameManager.Instance.uiManager.inventoryPanel;
+
         plantModeToggle.GetComponent<Toggle>().onValueChanged.AddListener((vlaue) =>
         {
             SwitchToHouseMode();
@@ -28,26 +40,32 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
         storePurchaseButton.onClick.AddListener(() =>
         {
             canClick = true;
-            foreach(var part in partObjects)
+
+            foreach (var part in partObjects)
             {
                 part.isClickable = false;
             }
+
             HH_GameManager.Instance.inputManager.OnObjectSelected -= OnPartTapped;
             bubble.gameObject.SetActive(false);
-            //Destroy(bubble.gameObject);
             ShowInventoryButton();
-            //OnTutorialStepComplete();
         });
+
         onClick.AddListener(ShowToggle);
     }
+
     public void ShowInventoryButton()
     {
-        FF_TutorialManager.Instance.tutorialText.text = "You can view your Owned Materials in the Inventory. Try tapping on the Inventory button to open the Inventory";
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(inventoryIntroKey);
+
         inventory.gameObject.SetActive(true);
+
         var inventoryBtn = inventory.inventoryButton;
         inventoryBtn.onClick.AddListener(OnInventoryOpened);
         inventoryBtn.gameObject.SetActive(true);
         inventoryBtn.interactable = false;
+
         Sequence sq = DOTween.Sequence();
         sq.Append(ScaleEffect(inventoryBtn.GetComponent<RectTransform>()));
         sq.onComplete += () =>
@@ -58,10 +76,12 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
 
     void OnInventoryOpened()
     {
-        FF_TutorialManager.Instance.tutorialText.text = "You can always tap on the item to replace the material. Try tapping on the Wood";
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(inventoryReplaceKey);
 
         oldItem = inventory.items[0];
         newItem = inventory.items[1];
+
         newItem.button.interactable = false;
         ScaleEffect(oldItem.GetComponent<RectTransform>());
         oldItem.button.onClick.AddListener(OnClickedOldItem);
@@ -70,7 +90,9 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
 
     void OnClickedNewItem()
     {
-        FF_TutorialManager.Instance.tutorialText.text = "Now your roof is using Metal. You can use the inventory anytime you want to apply other materials to your house!";
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(inventoryCompleteKey);
+
         inventory.gameObject.SetActive(false);
         Destroy(bubble.gameObject);
         onClick.AddListener(OnTutorialStepComplete);
@@ -79,39 +101,50 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
     void OnClickedOldItem()
     {
         newItem.button.interactable = true;
-        FF_TutorialManager.Instance.tutorialText.text = "Great job! Now try tapping on the Metal again";
+
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(inventoryRetryKey);
+
         oldItem.button.interactable = false;
         ScaleEffect(newItem.GetComponent<RectTransform>());
     }
+
     public void ShowToggle()
     {
         plantModeToggle.SetActive(true);
         onClick.RemoveAllListeners();
-        FF_TutorialManager.Instance.tutorialText.text = "Click on the toggle to switch to “House Mode”";
+
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(toggleHouseModeKey);
+
         Sequence toggleSequence = DOTween.Sequence();
         toggleSequence.Append(ScaleEffect(plantModeToggle.GetComponent<RectTransform>()));
         toggleSequence.onComplete += () =>
         {
             plantModeToggle.GetComponent<Toggle>().interactable = true;
         };
-
     }
+
     private Tween ScaleEffect(RectTransform rect)
     {
         return rect.DOScale(Vector3.one * 1.5f, 0.5f)
                          .SetLoops(4, LoopType.Yoyo)
                          .SetEase(Ease.InOutQuad);
     }
+
     void SwitchToHouseMode()
     {
         plantModeToggle.GetComponent<Toggle>().interactable = false;
         HH_GameManager.Instance.IsPlantMode = false;
-        FF_TutorialManager.Instance.tutorialText.text = "Let’s try upgrading the roof. Tap to see the roofing options.";
+
+        FF_TutorialManager.Instance.tutorialText.text =
+            StringManager.Instance.GetText(upgradeRoofIntroKey);
+
         canClick = false;
+
         HH_GameManager.Instance.cameraController.Zoomcamera(camH1, true, 60);
         houseManager = HH_GameManager.Instance.currentPlayer;
         partObjects = houseManager.GetAllHousePartObjectsOf(partType);
-
 
         foreach (var part in partObjects)
         {
@@ -124,18 +157,22 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
             }
             break;
         }
-        
+
         ShowUpgradeHousePart();
     }
+
     public void ShowUpgradeHousePart()
     {
         HH_GameManager.Instance.inputManager.OnObjectSelected += OnPartTapped;
+
         bubble.button.enabled = true;
         bubble.button.onClick.AddListener(() =>
         {
             OnPartTapped(gameObject);
         });
+
         highlightMesh.HighlightMeshes();
+
         DOVirtual.DelayedCall(1f, () =>
         {
             foreach (var part in partObjects)
@@ -143,28 +180,17 @@ public class FF_TutorializedHousePart : FF_TutorializedObject
                 part.isClickable = true;
                 highlightMesh.meshRenders.AddRange(part.meshes);
             }
-
         });
-
-
     }
-
-
 
     void OnPartTapped(GameObject obj)
     {
- 
         var part = obj.GetComponentInParent<BaseHousePartObject>();
-        if (part)
-        {         
-            if (part.HousePartType == partType)
-            {
-                FF_TutorialManager.Instance.tutorialText.text = "Each material is graded for fire resistance, with Class A being the highest rating. Let’s purchase this metal roofing.";
-            }
+
+        if (part && part.HousePartType == partType)
+        {
+            FF_TutorialManager.Instance.tutorialText.text =
+                StringManager.Instance.GetText(purchaseMaterialKey);
         }
-
     }
-
-
-
 }
