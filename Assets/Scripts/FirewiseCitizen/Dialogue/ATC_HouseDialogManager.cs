863d508e054a1e4de5bcae5748ba750a3b9e0485
Kieren <22388653+KierenE@users.noreply.github.com>
2026-01-15 20:43:34 -0800

    reinstating tts toggle in FC dialogue manager, clear playerprefs if ttstoggle left disabled between play tests

--- Assets/Scripts/FirewiseCitizen/Dialogue/ATC_HouseDialogManager.cs
+++ Assets/Scripts/FirewiseCitizen/Dialogue/ATC_HouseDialogManager.cs
@@ -255,8 +255,7 @@ public class ATC_HouseDialogManager : MonoBehaviour
             selectedAudioPath = currentNode.audioPathES;
         }
 
-        // if (!string.IsNullOrEmpty(selectedAudioPath) && TTSManager.IsEnabled)
-        if (!string.IsNullOrEmpty(selectedAudioPath))
+        if (!string.IsNullOrEmpty(selectedAudioPath) && TTSManager.IsEnabled)
         {
             AudioClip clip = Resources.Load<AudioClip>(selectedAudioPath);
             if (clip != null && audioSource != null)
