3f2aaa8a0af9f2454d4fc1993f8cbd1ed68c1a4e
tristynl <tristyn-lai@outlook.com>
2026-02-11 20:52:31 -0800

    Main Menu Settings Changes

--- Assets/Scripts/LanguageToggle.cs
+++ Assets/Scripts/LanguageToggle.cs
@@ -10,16 +10,47 @@ public class LanguageToggle : MonoBehaviour
         languageToggle = GetComponent<Toggle>();
     }
 
+    void OnEnable()
+    {
+        SyncToggleVisual(LocalizationManager.CurrentLanguage);
+        LocalizationManager.OnLanguageChanged += SyncToggleVisual;
+    }
+
+    void OnDisable()
+    {
+        LocalizationManager.OnLanguageChanged -= SyncToggleVisual;
+    }
+
     void Start()
     {
-        languageToggle.isOn = (LocalizationManager.CurrentLanguage == "es");
+        //languageToggle.isOn = (LocalizationManager.CurrentLanguage == "es");
 
+        languageToggle.onValueChanged.AddListener(HandleToggle);
+    }
+
+    private void SyncToggleVisual(string lang)
+    {
+        languageToggle.onValueChanged.RemoveListener(HandleToggle);
+        
+        languageToggle.isOn = (lang == "es");
+        
         languageToggle.onValueChanged.AddListener(HandleToggle);
     }
 
     void HandleToggle(bool isSpanish)
     {
+        string newLang = isSpanish ? "es" : "en";
+        
+        LocalizationManager.SetLanguage(newLang);
+
         LanguageSelector selector = Object.FindFirstObjectByType<LanguageSelector>();
+        if (selector != null)
+        {
+            if (isSpanish) selector.SetLanguageToSpanish();
+            else selector.SetLanguageToEnglish();
+        }
+        //For Reference
+        /*LanguageSelector selector = Object.FindFirstObjectByType<LanguageSelector>();
         if (selector != null)
         {
             if (isSpanish)
@@ -30,7 +61,7 @@ public class LanguageToggle : MonoBehaviour
         else
         {
             Debug.LogError("[ToggleConnector] Could not find LanguageSelector in the scene!");
-        }
+        }*/
     }
     
     private void OnDestroy()
--- Assets/Scripts/LocalizationManager.cs
+++ Assets/Scripts/LocalizationManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public static class LocalizationManager
 {
+    public static Action<string> OnLanguageChanged;
     public static string CurrentLanguage => PlayerPrefs.GetString("language", "en");
 
     public static void SetLanguage(string lang)
     {
         PlayerPrefs.SetString("language", lang);
         PlayerPrefs.Save();
+        OnLanguageChanged?.Invoke(lang);
     }
 }
